fa6e0aa82162a3145159a94281fa3fc80aa41b2b
Orange <whereorange@gmail.com>
2017-12-05 15:51:37 +0700

    Adding reworked. TODO: description

--- src/App.js
+++ src/App.js
@@ -1,43 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import List from './List';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state ={
-      term: '',
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
       items: []
     };
-  }
-  
-  onSubmit = (event) => {
-    event.preventDefault()//prevents form from submitting in my case
-    this.setState({
-      term: '',
-      items: [...this.state.items, this.state.term]
-    });
+ 
+    this.addItem = this.addItem.bind(this);
   }
 
-  onChange = (event) => {
-    this.setState({term: event.target.value});
+  addItem = (e) => {
+    var itemArray = this.state.items;
+
+    if(this._inputTaskElement.value !== ""){
+      itemArray.unshift(
+        {//todo: Description
+          text: this._inputTaskElement.value,
+          expDate: this._inputExpDateElement.value,
+          importance: this._inputImportanceElement.value,
+          key: Date.now()
+        }
+      )
+
+      this.setState({
+        items:itemArray
+      });
+
+      this._inputTaskElement.value = "";
+    };
+  console.log(itemArray); 
+  e.preventDefault();
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <form className="App" onSubmit={this.onSubmit}>
-            <input value={this.state.term} onChange={this.onChange} />
-            <button>Submit</button>
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input ref={(a) => this._inputTaskElement = a}
+               onChange={this.onChange} placeholder="enter task">
+            </input>
+            <input type="datetime-local"
+               ref={(a) => this._inputExpDateElement = a}
+               onChange={this.onChange} placeholder="enter exp date">
+            </input>
+            <select ref={(a) => this._inputImportanceElement = a}>
+              <option>Обычная</option>
+              <option>Важная</option>
+              <option>Очень важная</option>
+            </select>
+            <button type="submit">add</button>
           </form>
-          <List items={this.state.items}/>
-        </p>
+        </div>
       </div>
     );
   }
